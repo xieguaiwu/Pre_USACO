ccd8431733e3d130f3d9621dd7a6a47b1add7bf4
xieguaiwu <xieguaiwu@users.noreply.github.com>
2026-07-12 21:02:27 +0800

    清理 pointer_problems, 新增 basics

---
+++ basics/greedy/merging_fruits.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Chain {
+    int weight;
+    Chain *prev;
+    Chain *next;
+} Scheisse;
+
+int minWeight(&int sum, &int newheap[]) {
+    for (int i = 0; i < sum; ++i) {
+        
+    }
+    return;
+}
+
+int main() {
+	int sum;
+	cin >> sum;
+	int heap[sum];
+	for (int i = 0; i < sum; ++i) {
+		cin >> heap[i];
+	}
+    
+}
