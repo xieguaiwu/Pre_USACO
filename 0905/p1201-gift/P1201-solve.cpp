817de9bddf9f6bc1b6c00ed4c8ac8b0adba4707f
xieguaiwu <xieguaiwu@users.noreply.github.com>
2026-09-23 21:33:05 +0800

    chore: update P1201-solve and add review

--- 0905/p1201-gift/P1201-solve.cpp
+++ 0905/p1201-gift/P1201-solve.cpp
@@ -1,15 +1,34 @@
 #include <bits/stdc++.h>
-#include <vector>
 
 using namespace std;
 
 int main() {
 	int n;
 	cin >> n;
-	vector<string> names(n, nullptr);
-	vector<int> geben(n, 0);
-	vector<int> givenum(n, 0);
+	string sender = "";
+	string names[n];
+	string Nnames[n];
+	int geben = 0; //amount of money
+	int givenum = 0; // amount of people
+	map<string, int> total;
+
 	for (int i = 0; i < n; ++i) {
-		cin >> names[i] >> geben[i] >> givenum[i];
+		cin >> names[i];
+		total[names[i]] = 0;
+	}
+
+	for (int i = 0; i < n; ++i) {
+		cin >> sender >> geben >> givenum;
+		int avg = geben > 0 ? geben / givenum : 0;
+		int meingeld = geben > 0 ? geben - givenum * avg : 0;
+		total[sender] += meingeld - geben;
+		for (int j = 0; j < givenum; ++j) {
+			cin >> Nnames[j];
+			total[Nnames[j]] += avg;
+		}
+	}
+
+	for (int i = 0; i < n; ++i) {
+		cout << names[i] << " " << total[names[i]] << "\n";
 	}
 }
